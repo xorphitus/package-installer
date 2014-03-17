cdef85d0d4e327d5390d9cad1930b3c38d0fe801
xorphitus <xorphitus@gmail.com>
2014-03-18 00:20:19 +0900

    leinのprofileを追加

---
+++ README.md
@@ -0,0 +1,1 @@
+ln -s /path/to/this/dir/profiles.clj ~/.lein
---
+++ profiles.clj
@@ -0,0 +1,3 @@
+{:user {:dependencies [[slamhound "RELEASE"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :plugins [[lein-kibit "RELEASE"]]}}
