988b0a2b6889d3c928cf1531e0861d17b8ea04d6
xorphitus <xorphitus@gmail.com>
2014-04-09 00:22:35 +0900

    clojureのtraceを追加

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,4 @@
-{:user {:dependencies [[slamhound "RELEASE"]]
+{:user {:dependencies [[slamhound "RELEASE"]
+                       [org.clojure/tools.trace "RELEASE"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :plugins [[lein-kibit "RELEASE"]]}}
